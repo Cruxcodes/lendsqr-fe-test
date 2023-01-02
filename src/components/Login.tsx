8da725d6ec17f578f591f8b9e9ecd82740ed4889
ab <cruxCodes@gmail.com>
2023-01-02 14:36:28 +0100

    This is the initial typescript boiler plate

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/img/Group.png';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+        <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default Login
